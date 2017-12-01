b9d73f70bd0b1c292328a32da44643af77dcfcf2
Bendik Stavelin <bendik.stavelin@acando.no>
2017-12-01 10:32:50 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <input type="text" class="nameInput" v-model="name"/>
+      <input type="submit" class="nameSubmit" @click="submit"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Acando fagmøte 1 demo',
+        name: ''
+      }
+    },
+    methods: {
+      submit () {
+        console.log('name: ' + this.name)
+        axios.post('http://localhost:8081/addPerson', {name: this.name}).then(resp => {
+          console.log('person added, id: ' + resp.data.id)
+        })
+      }
+    },
+    mounted () {
+      console.log('mounted...')
+      axios.get('http://localhost:8081/hello').then(response => {
+        this.msg = response.data
+      })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
